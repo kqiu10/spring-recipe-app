fd5fb83d68060625081decdc4571fc8a8e1d462e
kqiu10 <qiukan97@gmail.com>
2021-01-25 12:11:49 -0500

    showed error data on 404 error page

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -71,7 +71,7 @@ public class RecipeControllerTest {
 
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
-        mockMvc.perform(get("/recipe/1/show"))
+        mockMvc.perform(get("/recipe/4/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
     }

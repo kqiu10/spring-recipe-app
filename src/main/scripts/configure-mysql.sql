e6ebe1689cc21ee831452aab13bb9e9aabf39385
kqiu10 <qiukan97@gmail.com>
2021-01-26 16:39:12 -0500

    refactored database initialization for MySQL

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -1,10 +1,10 @@
-## Use to run mysql dv docker image, optional if you're not using a local mysqldb
+## Use to run mysql db docker image, optional if you're not using a local mysqldb
 # docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
 
 # connect to mysql and run as root user
 #Create Databases
 CREATE DATABASE sfg_dev;
-CREATE DATABASE sfg-prod;
+CREATE DATABASE sfg_prod;
 
 #Create database service accounts
 CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
@@ -29,4 +29,3 @@ GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
 GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
 GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
 GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
-
